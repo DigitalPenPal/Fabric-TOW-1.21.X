71e5d41a59178ed00c05a70b1ba1bf9b2b8d55ab
DigitalPenPal <tamahei.tsing@gmail.com>
2025-04-05 21:14:48 -1000

    New stuff !

--- src/main/java/net/pencil/tow/entity/custom/NutcrackerSoldier1Entity.java
+++ src/main/java/net/pencil/tow/entity/custom/NutcrackerSoldier1Entity.java
@@ -21,22 +21,24 @@ public class NutcrackerSoldier1Entity extends HostileEntity {
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
 
-    protected NutcrackerSoldier1Entity(EntityType<? extends HostileEntity> entityType, World world) {
+    public NutcrackerSoldier1Entity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
     }
 
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new MeleeAttackGoal(this, 2.0, false));
-        this.goalSelector.add(1, new WanderNearTargetGoal(this, 0.9, 30.0f));
+        this.goalSelector.add(1, new WanderNearTargetGoal(this, 0.3, 30.0f));
         this.goalSelector.add(2, new WanderAroundFarGoal(this, 10));
 
         this.targetSelector.add(1, new RevengeGoal(this).setGroupRevenge());
-        this.targetSelector.add(2, new ActiveTargetGoal(this, MobEntity.class, 5, true,
-                true, entity -> entity instanceof Monster && !(entity instanceof NutcrackerSoldier1Entity)));
-        this.targetSelector.add(3, new ActiveTargetGoal(this, HostileEntity.class, true,
+        this.targetSelector.add(2, new ActiveTargetGoal(this, HostileEntity.class, 5, true,
+                true, entity -> !(entity instanceof NutcrackerSoldier1Entity)));
+        this.targetSelector.add(3, new ActiveTargetGoal(this, MobEntity.class, true,
                 true));
         this.targetSelector.add(4, new ActiveTargetGoal(this, GolemEntity.class, true,
+                true));
+        this.targetSelector.add(5, new ActiveTargetGoal(this, PlayerEntity.class, true,
                 true));
 
         this.goalSelector.add(2, new WanderAroundGoal(this, 0.3));
@@ -77,15 +79,5 @@ public class NutcrackerSoldier1Entity extends HostileEntity {
         return super.getNextAirUnderwater(air);
     }
 
-    @Override
-    protected void pushAway(Entity entity) {
 
-        if (entity instanceof Monster && (entity instanceof GolemEntity) && (entity instanceof HostileEntity) &&
-                !(entity instanceof NutcrackerSoldier1Entity) && this.getRandom().nextInt(20) == 0){
-
-            this.setTarget((LivingEntity) entity);
-        }
-
-        super.pushAway(entity);
-    }
 }
