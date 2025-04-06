2e1b8e3be45c3bf3509b42b2d3093b5b5986b9dd
DigitalPenPal <tamahei.tsing@gmail.com>
2025-04-05 20:54:25 -1000

    New stuff !

--- src/main/java/net/pencil/tow/entity/custom/NutcrackerSoldier1Entity.java
+++ src/main/java/net/pencil/tow/entity/custom/NutcrackerSoldier1Entity.java
@@ -2,8 +2,12 @@ package net.pencil.tow.entity.custom;
 
 
 import net.minecraft.entity.AnimationState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
@@ -38,5 +42,50 @@ public class NutcrackerSoldier1Entity extends HostileEntity {
         this.goalSelector.add(2, new WanderAroundGoal(this, 0.3));
         this.goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 5.0f));
         this.goalSelector.add(4, new LookAroundGoal(this));
+    }
+
+    public static DefaultAttributeContainer.Builder createAttributes() {
+        return MobEntity.createMobAttributes()
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 40)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3)
+                .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 1.0)
+                .add(EntityAttributes.GENERIC_SAFE_FALL_DISTANCE, 50)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 30)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 15.5);
+    }
+
+    private void setupAnimationStates() {
+        if (this.idleAnimationTimeout <= 0) {
+            this.idleAnimationTimeout = 60;
+            this.idleAnimationState.start(this.age);
+        } else {
+            --this.idleAnimationTimeout;
+        }
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (this.getWorld().isClient()) {
+            this.setupAnimationStates();
+        }
+    }
+
+    @Override
+    protected int getNextAirUnderwater(int air) {
+        return super.getNextAirUnderwater(air);
+    }
+
+    @Override
+    protected void pushAway(Entity entity) {
+
+        if (entity instanceof Monster && (entity instanceof GolemEntity) && (entity instanceof HostileEntity) &&
+                !(entity instanceof NutcrackerSoldier1Entity) && this.getRandom().nextInt(20) == 0){
+
+            this.setTarget((LivingEntity) entity);
+        }
+
+        super.pushAway(entity);
     }
 }
